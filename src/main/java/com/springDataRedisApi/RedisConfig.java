04f54a05206be35b9166cf961f70c60e498cf5d7
Azeez Ridwan <readone.cybernet@gmail.com>
2023-12-24 04:18:31 +0100

    update 1.1

--- src/main/java/com/springDataRedisApi/RedisConfig.java
+++ src/main/java/com/springDataRedisApi/RedisConfig.java
@@ -1,6 +1,9 @@
 package com.springDataRedisApi;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.data.redis.connection.RedisStandaloneConfiguration;
+import org.springframework.data.redis.connection.jedis.JedisConnectionFactory;
 import org.springframework.data.redis.repository.configuration.EnableRedisRepositories;
 
 /**
@@ -11,5 +14,16 @@ import org.springframework.data.redis.repository.configuration.EnableRedisReposi
 @Configuration
 @EnableRedisRepositories
 public class RedisConfig {
+	
+	@Bean
+	public JedisConnectionFactory jedisConnectionFactory() {
+		
+		RedisStandaloneConfiguration configuration = new RedisStandaloneConfiguration();
+		configuration.setHostName("localhost");
+		configuration.setPort(6379);
+		
+		return new JedisConnectionFactory(configuration);
+		
+	}
 
 }

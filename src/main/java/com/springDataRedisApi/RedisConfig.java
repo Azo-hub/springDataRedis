7316336f578858cce9a1e7b844b1244fd648c9c8
Azeez Ridwan <readone.cybernet@gmail.com>
2023-12-24 21:00:31 +0100

    update 1.2

--- src/main/java/com/springDataRedisApi/RedisConfig.java
+++ src/main/java/com/springDataRedisApi/RedisConfig.java
@@ -4,7 +4,10 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.redis.connection.RedisStandaloneConfiguration;
 import org.springframework.data.redis.connection.jedis.JedisConnectionFactory;
+import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.data.redis.repository.configuration.EnableRedisRepositories;
+import org.springframework.data.redis.serializer.JdkSerializationRedisSerializer;
+import org.springframework.data.redis.serializer.StringRedisSerializer;
 
 /**
  * @author Azo-hub
@@ -16,13 +19,28 @@ import org.springframework.data.redis.repository.configuration.EnableRedisReposi
 public class RedisConfig {
 	
 	@Bean
-	public JedisConnectionFactory jedisConnectionFactory() {
+	JedisConnectionFactory jedisConnectionFactory() {
 		
 		RedisStandaloneConfiguration configuration = new RedisStandaloneConfiguration();
 		configuration.setHostName("localhost");
 		configuration.setPort(6379);
 		
 		return new JedisConnectionFactory(configuration);
+		
+	}
+	
+	@Bean
+	RedisTemplate<String, Object> redisTemplate() {
+		
+		RedisTemplate<String, Object> template = new RedisTemplate<>();
+		template.setConnectionFactory(jedisConnectionFactory());
+		template.setKeySerializer(new StringRedisSerializer());
+		template.setHashKeySerializer(new StringRedisSerializer());
+		template.setHashKeySerializer(new JdkSerializationRedisSerializer());
+		template.setValueSerializer(new JdkSerializationRedisSerializer());
+		template.setEnableTransactionSupport(true);
+		template.afterPropertiesSet();
+		return template;
 		
 	}
 

ac895d39467ec663328213ce5cc00b5ce5df370e
Azeez Ridwan <readone.cybernet@gmail.com>
2023-12-24 03:48:33 +0100

    update 1.0

---
+++ src/main/java/com/springDataRedisApi/Entity/Product.java
@@ -0,0 +1,51 @@
+package com.springDataRedisApi.Entity;
+
+import org.springframework.data.annotation.Id;
+import org.springframework.data.redis.core.RedisHash;
+
+/**
+ * @author Azo-hub
+ * @github (https://github.com/Azo-hub)
+ * @since 2020
+ */
+@RedisHash("product")
+public class Product {
+	@Id
+	private Long id;
+	private String name;
+	private int qty;
+	private double price;
+	
+	public Long getId() {
+		return id;
+	}
+	
+	public void setId(Long id) {
+		this.id = id;
+	}
+	
+	public String getName() {
+		return name;
+	}
+	
+	public void setName(String name) {
+		this.name = name;
+	}
+	
+	public int getQty() {
+		return qty;
+	}
+	
+	public void setQty(int qty) {
+		this.qty = qty;
+	}
+	
+	public double getPrice() {
+		return price;
+	}
+	
+	public void setPrice(double price) {
+		this.price = price;
+	}
+
+}
---
+++ src/main/java/com/springDataRedisApi/RedisConfig.java
@@ -0,0 +1,15 @@
+package com.springDataRedisApi;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.redis.repository.configuration.EnableRedisRepositories;
+
+/**
+ * @author Azo-hub
+ * @github (https://github.com/Azo-hub)
+ * @since 2020
+ */
+@Configuration
+@EnableRedisRepositories
+public class RedisConfig {
+
+}
